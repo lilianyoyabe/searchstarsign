0f014a449428fdae539e9e2c14bbda2b9ec4ba7f
YU,I-JUI <lilianyoyabe@gmail.com>
2023-10-27 00:20:32 +0800

    version2
    without select2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,25 @@ $(function() {
 	$('.js-multiple-select').select2({
 		tags: true,
 		tokenSeparators: [',', ' '],
-		placeholder: 'Select a state'
+		placeholder: '請選擇一個或多個星座'
 	});
 
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let multipleSelect = document.getElementById("multipleSelect");
+    let nameList = document.getElementById("nameList");
+
+    multipleSelect.addEventListener("change", function () {
+        nameList.innerHTML = "";
+        let selectedStarSigns = Array.from(multipleSelect.selectedOptions).map(option => option.value);
+        console.log(selectedStarSigns);
+        for (let i = 0; i < data.length; i++) {
+            if (selectedStarSigns.includes(data[i].starSign)) {
+                let listItem = document.createElement("li");
+                listItem.textContent = data[i].name;
+                nameList.appendChild(listItem);
+            }
+        }
+    });
 });
